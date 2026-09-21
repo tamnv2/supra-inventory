3f70c1eeabaa85ec132a7bd6e7fa5856d579abe1
tamnv2 <nguyenvantam050595@gmail.com>
2026-09-21 12:47:49 +0700

    D097: add Firestore listener dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -70,6 +70,8 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.19.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-messaging")
     implementation("androidx.core:core:1.17.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
