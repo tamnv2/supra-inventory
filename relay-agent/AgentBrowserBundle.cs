5980dc7ec7a11324f8b39a700f80252dcb2151ec
tamnv2 <nguyenvantam050595@gmail.com>
2026-09-27 02:40:43 +0700

    D127 v60: require browser host build 5

--- relay-agent/AgentBrowserBundle.cs
+++ relay-agent/AgentBrowserBundle.cs
@@ -23,7 +23,7 @@ namespace SupraInventoryRelayAgent
 
         private static readonly JavaScriptSerializer Json = new JavaScriptSerializer();
         private static readonly object StateLock = new object();
-        private const int RequiredHostBuild = 4;
+        private const int RequiredHostBuild = 5;
         private const string RequiredHostArch = "x64";
         private static int _backgroundRunning;
         private static int _cleanupScheduled;
